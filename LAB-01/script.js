be661da2e8a3c0855ce71b088e467220a2b4a5a3
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-10-04 18:13:37 +0200

    Refacotr js functions

--- LAB-01/script.js
+++ LAB-01/script.js
@@ -1,59 +1,39 @@
-document.querySelector("#calculate").addEventListener('click', shame);
-
 // Utwórz w html cztery pola tekstowe i przycisk "Przelicz"
 // Po naciśnięciu "Przelicz" pokaż na stronie sumę, średnią, min i max z wartości wpisanych do pól tekstowych.
+const btn = document.querySelector(".calculate");
+
+const field1 = parseInt(document.querySelector(".field1").value);
+const field2 = parseInt(document.querySelector(".field2").value);
+const field3 = parseInt(document.querySelector(".field3").value);
+const field4 = parseInt(document.querySelector(".field4").value);
+
+for(let a = 1; a <= 10; a++){
+  console.log(a);
+}
 
 function shame(){
-    const a = parseInt(document.querySelector("#field1").value);
-    const b = parseInt(document.querySelector("#field2").value);
-    const c = parseInt(document.querySelector("#field3").value);
-    const d = parseInt(document.querySelector("#field4").value);
-
-    const sum = a + b + c + d;
-    const avg = (a + b + c + d)/4;
-    const minimum = Math.min(a, b, c, d)
-    const maximum = Math.max(a, b, c, d);
-
-    document.querySelector("#sum").innerHTML = sum; 
-    document.querySelector("#avg").innerHTML = avg; 
-    document.querySelector("#min").innerHTML = minimum; 
-    document.querySelector("#max").innerHTML = maximum; 
+  let sum = field1 + field2 + field3 + field4;
+  let avg = (field1 + field2 + field3 + field4)/4;
+  let minimum = Math.min(field1, field2 ,field3, field4)
+  let maximum = Math.max(field1, field2, field3, field4);
+    
+  document.querySelector(".sum").innerHTML = sum; 
+  document.querySelector(".avg").innerHTML = avg; 
+  document.querySelector(".min").innerHTML = minimum; 
+  document.querySelector(".max").innerHTML = maximum; 
 }
 
 //Wyliczone wartości pokazuj live gdy tylko użytkownik zmieni wartość w polu tekstowym
+// function zieww(e){
+//     const sum = a + b + c + d;
+//     const avg = (a + b + c + d)/4;
+//     const minimum = Math.min(a, b, c, d)
+//     const maximum = Math.max(a, b, c, d);
 
+//     a1 = document.querySelector(".sum").innerHTML = sum;
+//     a1.textContent = e.target.value;
+// }
 
-
-const input = document.querySelector('input');
-const log = document.getElementById('log');
-
-input.addEventListener('change', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
-
-const liczby = document.querySelector(".calculator");
-const a1 = document.querySelector("#sum"); 
-const a2 = document.querySelector("#avg");
-const a3 = document.querySelector("#min");
-const a4 = document.querySelector("#max");
-
-liczby.addEventListener('change', zieww);
-
-function zieww(e){
-    const a = parseInt(document.querySelector("#field1").value);
-    const b = parseInt(document.querySelector("#field2").value);
-    const c = parseInt(document.querySelector("#field3").value);
-    const d = parseInt(document.querySelector("#field4").value);
-
-    const sum = a + b + c + d;
-    const avg = (a + b + c + d)/4;
-    const minimum = Math.min(a, b, c, d)
-    const maximum = Math.max(a, b, c, d);
-
-    a1 = document.querySelector("#sum").innerHTML = sum;
-    a1.textContent = e.target.value;
-
-
-}
+btn.addEventListener('click', shame);
+console.log();
+// numbers.addEventListener('change', zieww);
