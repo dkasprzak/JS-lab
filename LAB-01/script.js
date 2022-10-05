e54dcd8cbde57274cb62132cd172147f1ee0a3b7
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-10-05 18:29:11 +0200

    refactor shame function

--- LAB-01/script.js
+++ LAB-01/script.js
@@ -1,21 +1,41 @@
 // Utwórz w html cztery pola tekstowe i przycisk "Przelicz"
 // Po naciśnięciu "Przelicz" pokaż na stronie sumę, średnią, min i max z wartości wpisanych do pól tekstowych.
 const btn = document.querySelector(".calculate");
-const field1 = parseInt(document.querySelector(".field1").value);
-const field2 = parseInt(document.querySelector(".field2").value);
-const field3 = parseInt(document.querySelector(".field3").value);
-const field4 = parseInt(document.querySelector(".field4").value);
 
-function shame(){
-  let sum = field1 + field2 + field3 + field4;
-  let avg = (field1 + field2 + field3 + field4)/4;
-  let minimum = Math.min(field1, field2 ,field3, field4)
-  let maximum = Math.max(field1, field2, field3, field4);
-    
-  document.querySelector(".sum").innerHTML = sum; 
-  document.querySelector(".avg").innerHTML = avg; 
-  document.querySelector(".min").innerHTML = minimum; 
-  document.querySelector(".max").innerHTML = maximum; 
+const field1 = document.querySelector(".field1");
+const field2 = document.querySelector(".field2");
+const field3 = document.querySelector(".field3");
+const field4 = document.querySelector(".field4");
+
+const sumField = document.querySelector(".sum"); 
+const avgField = document.querySelector(".avg"); 
+const minField = document.querySelector(".min"); 
+const maxField = document.querySelector(".max"); 
+
+const shame = () =>{
+  let sum = +field1.value + +field2.value + +field3.value + +field4.value;
+  
+  let avg = sum / 4;
+  
+  let minimum = Math.min(
+    field1.value, 
+    field2.value,
+    field3.value, 
+    field4.value
+  );
+
+  let maximum = Math.max(
+    field1.value, 
+    field2.value,
+    field3.value, 
+    field4.value
+  );
+
+  sumField.innerHTML = sum;
+  avgField.innerHTML = avg;
+  minField.innerHTML = minimum;
+  maxField.innerHTML = maximum;
+  
 }
 
 //Wyliczone wartości pokazuj live gdy tylko użytkownik zmieni wartość w polu tekstowym
