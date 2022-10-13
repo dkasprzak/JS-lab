6350e0a711dfbc94d444a36d91fedb4afff2415f
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-10-13 17:42:59 +0200

    Create button and simple function to create new input

--- LAB-01/script.js
+++ LAB-01/script.js
@@ -1,11 +1,12 @@
 // Utwórz w html cztery pola tekstowe i przycisk "Przelicz"
 // Po naciśnięciu "Przelicz" pokaż na stronie sumę, średnią, min i max z wartości wpisanych do pól tekstowych.
 const btn = document.querySelector(".calculate");
+const addField = document.querySelector(".addField");
 
+const form = document.querySelector(".calculator");
 const field1 = document.querySelector(".field1");
 const field2 = document.querySelector(".field2");
 const field3 = document.querySelector(".field3");
-const field4 = document.querySelector(".field4");
 
 const sumField = document.querySelector(".sum"); 
 const avgField = document.querySelector(".avg"); 
@@ -13,22 +14,20 @@ const minField = document.querySelector(".min");
 const maxField = document.querySelector(".max"); 
 
 const shame = () =>{
-  let sum = +field1.value + +field2.value + +field3.value + +field4.value;
+  let sum = +field1.value + +field2.value + +field3.value;
   
-  let avg = sum / 4;
+  let avg = sum / 3;
   
   let minimum = Math.min(
     field1.value, 
     field2.value,
     field3.value, 
-    field4.value
   );
 
   let maximum = Math.max(
     field1.value, 
     field2.value,
     field3.value, 
-    field4.value
   );
 
   sumField.innerHTML = sum;
@@ -43,22 +42,20 @@ const shame = () =>{
 const fields = document.querySelector(".calculator");
 
 const zieww = () =>{
-  let sum = +field1.value + +field2.value + +field3.value + +field4.value;
+  let sum = +field1.value + +field2.value + +field3.value;
   
-  let avg = sum / 4;
+  let avg = sum / 3;
   
   let minimum = Math.min(
     field1.value, 
     field2.value,
     field3.value, 
-    field4.value
   );
 
   let maximum = Math.max(
     field1.value, 
     field2.value,
     field3.value, 
-    field4.value
   );
 
     sumField.textContent = sum;
@@ -67,5 +64,12 @@ const zieww = () =>{
     maxField.textContent = maximum;
 }
 
+const normal = () =>{
+  var createInput = document.createElement("input");
+  createInput.setAttribute("type", "text");
+  form.appendChild(createInput);
+}
+
 //btn.addEventListener('click', shame);
+addField.addEventListener('click', normal)
 fields.addEventListener('change', zieww);
