e60b219a326a94b3713a3875a953072ccb4619c0
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-11-07 18:00:25 +0100

    Add styles for drumkit, Bind upper cases in script

--- LAB-03/script.js
+++ LAB-03/script.js
@@ -2,14 +2,23 @@ document.addEventListener('keypress', onKeyPress);
 
 const KeyToSound = {
     'a': document.querySelector('#clap'),
+    'A': document.querySelector('#clap'),
     's': document.querySelector('#boom'),
+    'S': document.querySelector('#boom'),
     'd': document.querySelector('#hihat'),
+    'D': document.querySelector('#hihat'),
     'f': document.querySelector('#kick'),
+    'F': document.querySelector('#kick'),
     'g': document.querySelector('#openhat'),
+    'G': document.querySelector('#openhat'),
     'h': document.querySelector('#ride'),
+    'H': document.querySelector('#ride'),
     'j': document.querySelector('#snare'),
+    'J': document.querySelector('#snare'),
     'k': document.querySelector('#tink'),
-    'l': document.querySelector('#tom')
+    'K': document.querySelector('#tink'),
+    'l': document.querySelector('#tom'),
+    'L': document.querySelector('#tom')
 }
 
 function onKeyPress(event){
