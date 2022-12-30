dc5e5ccda1c7943f118c9086ccd3444d08ed6e7d
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-12-30 15:57:26 +0100

    add Note class with methods

--- LAB-04/script.js
+++ LAB-04/script.js
@@ -1,19 +1,60 @@
-const addButton = document.querySelector("#add");
-const notes = document.querySelector(".notes");
+class NotePocket{
+    constructor (title, content, color, pin, createdDate){
+        this.title = title;
+        this.content = content;
+        this.color = color;
+        this.pin = pin;
+        this.createdDate = createdDate;
+        this.notes = [];
+    }
 
-function addNote(){
-    const newNote = document.createElement("div");
-    newNote.classList.add("addedNote");
-    const title = document.createElement("input");
-    const content = document.createElement("textarea");
-    const editButton = document.createElement("button"); 
-    editButton.innerHTML = "Edit";
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    newNote.appendChild(title);
-    newNote.appendChild(content);
-    newNote.appendChild(editButton);
-    newNote.appendChild(deleteButton);
-    notes.appendChild(newNote);
+    addNote = () => {
+        console.log("Dodawanie nowej notatki");
+        const title = document.querySelector("#title").value;
+        const content = document.querySelector("#content").value;
+        const color = document.querySelector("#color").value;
+        const pin = document.querySelector("#pin").checked;
+        const createdDate = new Date();
+
+        const newNote = new NotePocket(title, content, color, pin, createdDate);
+
+        const notesArray = JSON.parse(localStorage.getItem('notes')) || [];
+        notesArray.push(newNote);
+        localStorage.setItem('notes', JSON.stringify(notesArray));
+
+        displayNotes();
+    }
 }
-addButton.addEventListener('click', addNote);
+
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", () => {
+  new NotePocket().addNote();
+});
+
+function displayNotes(){
+    const notesArray = JSON.parse(localStorage.getItem('notes')) || [];
+
+    const notes = document.querySelector(".notes");
+    notes.innerHTML = "";
+
+    for(let i = 0; i < notesArray.length; i++){
+        const note = notesArray[i];
+
+        const noteDiv = document.createElement("div");
+        noteDiv.className = "note";
+
+        const noteTitle = document.createElement("h3");
+        noteTitle.textContent = note.title;
+
+        noteDiv.style.backgroundColor = note.color;
+
+        const noteContent = document.createElement("p");
+        noteContent.textContent = note.content;
+
+        noteDiv.appendChild(noteTitle);
+        noteDiv.appendChild(noteContent);
+        notes.appendChild(noteDiv);
+    }
+}
+
+displayNotes();
