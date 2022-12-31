14149e9f30c01dd1d91ac2661f772fd31ee4bf16
dkasprzak <dominik.kasprzak1408@gmail.com>
2022-12-31 16:44:12 +0100

    add delete, and edit notes methods

--- LAB-04/script.js
+++ LAB-04/script.js
@@ -9,7 +9,6 @@ class NotePocket{
     }
 
     addNote = () => {
-        console.log("Dodawanie nowej notatki");
         const title = document.querySelector("#title").value;
         const content = document.querySelector("#content").value;
         const color = document.querySelector("#color").value;
@@ -21,8 +20,7 @@ class NotePocket{
         const notesArray = JSON.parse(localStorage.getItem('notes')) || [];
         notesArray.push(newNote);
         localStorage.setItem('notes', JSON.stringify(notesArray));
-
-        displayNotes();
+        displayNotes();  
     }
 }
 
@@ -51,8 +49,66 @@ function displayNotes(){
         const noteContent = document.createElement("p");
         noteContent.textContent = note.content;
 
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "delete";
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.className = "edit";
+
+        editBtn.addEventListener("click", () => {
+            const title = note.title;
+            const content = note.content;
+            const color = note.color;
+
+            const editForm = document.createElement("form");
+            editForm.innerHTML = `<input type = "text" id = "edit-title" value="${title}" required>
+            <textarea id="edit-content">${content}</textarea> <select id="edit-color" required> 
+            <option value="">Select color...</option> <option value="#43B9B3" ${color === 
+            "#43B9B3" ? "selected" : ""}>Blue</option> <option value="#B343B9" ${color ===   
+            "#B343B9" ? "selected" : ""}>Violet</option> <option value="#B9B343" ${color ===
+            "#B9B343" ? "selected" : ""}>Yellow</option> </select> <button id="save-edit">Save</button> <button id="cancel-edit">Cancel</button> `;
+             editForm.id = "edit-form";       
+
+            editForm.querySelector("#save-edit").addEventListener("click", () => {
+              const newTitle = editForm.querySelector("#edit-title").value;
+              const newContent = editForm.querySelector("#edit-content").value;
+              const newColor = editForm.querySelector("#edit-color").value;
+          
+              notesArray[i].title = newTitle;
+              notesArray[i].content = newContent;
+              notesArray[i].color = newColor;
+          
+              localStorage.setItem('notes', JSON.stringify(notesArray));
+          
+              noteDiv.removeChild(editForm);
+              displayNotes();
+            });
+          
+            editForm.querySelector("#cancel-edit").addEventListener("click", () => {
+              noteDiv.removeChild(editForm);
+            });
+
+            noteDiv.appendChild(editForm);
+            editBtn.addEventListener("click", () => {
+            noteDiv.removeChild(editBtn);
+            });
+    
+        });
+
+        deleteBtn.addEventListener("click", () =>{
+            notesArray.splice(i, 1);
+
+            //aktualizacja localStorage
+            localStorage.setItem('notes', JSON.stringify(notesArray));
+            displayNotes();
+        });
+
         noteDiv.appendChild(noteTitle);
         noteDiv.appendChild(noteContent);
+        noteDiv.appendChild(deleteBtn);
+        noteDiv.appendChild(editBtn);
         notes.appendChild(noteDiv);
     }
 }
