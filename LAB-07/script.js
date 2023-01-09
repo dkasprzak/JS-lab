0ec343e03405b075830dfb1d2a5b1c7a6a71a303
dkasprzak <dominik.kasprzak1408@gmail.com>
2023-01-09 19:38:27 +0100

    add template

--- LAB-07/script.js
+++ LAB-07/script.js
@@ -1,9 +1,30 @@
 const searchCity = document.querySelector("#searchCity");
 const searchBtn = document.querySelector("#searchBtn");
+const output = document.querySelector(".wather-section");
 
-searchBtn.addEventListener('click', function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&units=metric" + "&APPID=565e4d8719359b6e425d0db62d742927")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => alert("Wrong city name"));
+const API_KEY = "70a93eb9cb351bc0b11a03f8778d2dd6";
+
+const getWeather = (city) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          const temperature = response.main.temp;
+          output.innerHTML = `${temperature}`;
+        } else {
+          showError("An error occurred while getting the weather data.");
+        }
+    };
+    xhr.open(
+      "GET",
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    xhr.send();
+  };
+  
+ 
+
+searchBtn.addEventListener('click', () => {
+    const city = searchCity.value;
+    getWeather(city);
 });
