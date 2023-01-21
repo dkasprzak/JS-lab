28fe92e2c6ee252a30fd95819e2ddd34ac8922b0
dkasprzak <dominik.kasprzak1408@gmail.com>
2023-01-21 13:56:40 +0100

    use fetch

--- LAB-07/script.js
+++ LAB-07/script.js
@@ -4,23 +4,17 @@ const output = document.querySelector(".wather-section");
 
 const API_KEY = "70a93eb9cb351bc0b11a03f8778d2dd6";
 
-const getWeather = (city) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          const temperature = response.main.temp;
-          output.innerHTML = `${temperature}`;
-        } else {
-          showError("An error occurred while getting the weather data.");
-        }
-    };
-    xhr.open(
-      "GET",
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    xhr.send();
-  };
+const getWeather = async (city) => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+    const temperature = data.main.temp;
+    output.innerHTML = `${temperature}`;
+  })
+  .catch(error => {
+    showError("Connect issue")
+  });
+};
   
  
 
