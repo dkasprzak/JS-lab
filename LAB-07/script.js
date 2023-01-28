4b8d5360645ab96372953e53b4850acae1af3446
dkasprzak <dominik.kasprzak1408@gmail.com>
2023-01-28 13:47:28 +0100

    redesign page

--- LAB-07/script.js
+++ LAB-07/script.js
@@ -1,6 +1,6 @@
 const searchCity = document.querySelector("#searchCity");
 const searchBtn = document.querySelector("#searchBtn");
-const output = document.querySelector(".wather-section");
+const output = document.querySelector(".weather-content");
 
 const API_KEY = '5e126d96c061c518602f08ff5a8fe77e';
 
@@ -9,18 +9,33 @@ const getWeather = (city) => {
   .then(response => response.json())
   .then(data => {
 
-    const temperature = Math.round(data.main.temp - 272.15);
-    const city = data.name;
-    output.innerHTML = `
-    City: ${city} <br>
-    Temperature: ${temperature} &#8451   
-    `;
+    let weatherIcon = data.weather[0].icon;
+    let city = data.name;
+    let temperature = Math.round(data.main.temp - 272.15);
+    let humidity = data.main.humidity;
+    const watherContentChild = document.createElement('div');
+    watherContentChild.className = "wetaher-content-child";
+    watherContentChild.innerHTML = `
+      <div class="delete">
+        <img src="./icons/icon-close.png" alt="close-icon"/>
+      </div>
+      <div class="city-title">
+        <h2>${city}</h2>
+      </div>
+      <div class="weather">
+        <img  class="weatherIcon" src="" alt="icon"/>
+        <span class="temperature">${temperature} &#x2103;</span></br>
+        <span class="humidity">Humidity: ${humidity}&#x25;</span>
+      </div>  
+    `
+    watherContentChild.querySelector('.weatherIcon').src=`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    output.appendChild(watherContentChild);  
+
   })
   .catch(error => {
     showError("Connect issue")
   });
 };
-  
  
 
 searchBtn.addEventListener('click', (e) => {  
